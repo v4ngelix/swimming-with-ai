15897862da6e8262beaca13222a635e1f8b787eb
Andi Angel <andi.angel@boheemia.ee>
2026-09-20 21:30:04 +0300

    Fix deploy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ const { version } = JSON.parse(readFileSync('./package.json', 'utf8'));
 
 export default defineConfig({
   server: { open: true },
+  base: './',
   build: { outDir: 'dist', emptyOutDir: true },
   define: { __APP_VERSION__: JSON.stringify(version) },
 });
